d47a82e7fc1e542d2246e329b597e1922876feb9
TomJB1 <tombrandisicloud@gmail.com>
2023-03-20 20:50:16 +0000

    More images load when one scrolls down to the bottom of the page
    still not great on mobile

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-numberofImages = 8
+numberofImages = 20
 
+const main = document.getElementById("main");
 
+const loader = document.getElementById("loader");
+
+const imgIncrease = 4;
+
+let currentPage = 1;
+
+const pageCount = Math.ceil(numberofImages / imgIncrease);
+
+const InfiniteScroll = () => {
+    const endOfPage = window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+    if (endOfPage) {
+      LoadPage(currentPage + 1);
+    }
+};
+
+window.addEventListener("scroll", InfiniteScroll);
 
 function AddImage(src) {
     var img = new Image();
@@ -18,9 +35,26 @@ function MakeDiv(id)
     document.getElementById('main').appendChild(div);
 }
 
-for (let i = 1; i < numberofImages; i++) {
+function LoadPage(page)
+{
+    currentPage = page;
+
+    const startRange = (page - 1) * imgIncrease;
+    const endRange = page * imgIncrease;
+
+    for (let i = startRange; i < endRange; i++) {
     src = "images/" + i + ".jpg"
     console.log(i)
     console.log(src)
     AddImage(src);
+    }
 }
+
+
+
+
+window.onload = function () {
+    LoadPage(currentPage);
+    LoadPage(currentPage+1)
+    LoadPage(currentPage+2)
+  };
