6a349e30c5c02e225f59b362104dbb63a686b31c
Tom <tombrandisicloud@gmail.com>
2023-06-20 11:16:08 +0100

    test of password

--- echo-password.php
+++ echo-password.php
@@ -3,9 +3,10 @@
 <body>
 
     <?php
-    if($_POST["password"] == 'secret')
-        echo "pass"; 
-    ?><br>
+    if($_POST["password"] == 'secret'): ?>
+        <h1>access allowed</h1>
+    <?php else: ?>
+        <h1>access denied</h1>
 
 </body>
 </html> 
