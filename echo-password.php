60a291f0cfed657d2c53d85932fc36be20387965
Tom <tombrandisicloud@gmail.com>
2023-06-20 11:19:49 +0100

    fix

--- echo-password.php
+++ echo-password.php
@@ -3,7 +3,7 @@
 <body>
 
     <?php
-    if($_POST["password"] == 'secret'): ?>
+    if('secret' == $_POST["password"]): ?>
         <h1>access allowed</h1>
     <?php else: ?>
         <h1>access denied</h1>
