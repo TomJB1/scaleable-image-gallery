fa3748684f9134d735c43228e5b604d99f91d783
Tom <tombrandisicloud@gmail.com>
2023-06-26 13:06:56 +0100

    fix

--- authenticate.php
+++ authenticate.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-if('secret' == $_POST["password"]): ?>
-
-<?php $_SESSION['logged_on'] = true; 
-header("Location: gallery.php"); ?>
-
+if('secret' == $_POST["password"])
+{
+    $_SESSION['logged_on'] = true; 
+    header("Location: gallery.php");
+}?>
 
 <?php else: ?>
 
