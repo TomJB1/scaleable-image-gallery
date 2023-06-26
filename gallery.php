29f08d30add1dbf66b40a14632a96425660a7605
Tom <tombrandisicloud@gmail.com>
2023-06-26 15:55:53 +0100

    dont load script when not authenticated

--- gallery.php
+++ gallery.php
@@ -5,12 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+    
+    if('secret' == $_POST["password"]): ?>
+    
     <link rel="stylesheet" type="text/css" href="styles.css" />
     <script src="script.js" defer></script>
 </head>
 <body id="body">
-    <?php
-    if('secret' == $_POST["password"]): ?>
     <div id="header">
         <h1>Matt Brandis - Photo Gallery</h1>
     </div>
@@ -18,6 +20,9 @@
             
     </div>
     <?php else: ?>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body id="body">
         <h1>login failed</h1>
         <a href="login.php">Login</a>
     <?php endif ?>
