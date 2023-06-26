7b582cfb1f8f624c9ea9497c06570cd4fa0afecd
Tom <tombrandisicloud@gmail.com>
2023-06-26 13:12:34 +0100

    fix

--- authenticate.php
+++ authenticate.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 session_start();
 if('secret' == $_POST["password"]):?>
@@ -10,3 +19,5 @@ if('secret' == $_POST["password"]):?>
 
 <?php endif ?>
 <h1>Test</h1>
+<body>
+</html>
