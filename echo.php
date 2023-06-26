2dc31bbc50ff9e39abfd584d59396e6d47c40df7
Tom <tombrandisicloud@gmail.com>
2023-06-26 19:46:41 +0100

    fix

--- echo.php
+++ echo.php
@@ -12,9 +12,7 @@
 </form>
 <?php
 echo password_hash($_POST["password"], PASSWORD_DEFAULT)
-?>
-<?php if(password_hash($_POST["password"], PASSWORD_DEFAULT) == '$2y$10$5iAdKxZNcXQCjwBOGomVC.2XT0E6ozYzxOc71qwjAZHDh6KUz/fzG')
-{
+if(password_hash($_POST["password"], PASSWORD_DEFAULT) == '$2y$10$5iAdKxZNcXQCjwBOGomVC.2XT0E6ozYzxOc71qwjAZHDh6KUz/fzG'){
     echo 'working'
 }?>
 </body>
