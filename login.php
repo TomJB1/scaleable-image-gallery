d7c59ecdd7b8432e5195bdc68ce3f15e9c811b9a
Tom <tombrandisicloud@gmail.com>
2023-06-26 15:10:55 +0100

    test

--- login.php
+++ login.php
@@ -1,15 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+
+if(isset($_POST['submit_pass']) && $_POST['pass'])
+{
+ $pass=$_POST['pass'];
+ if($pass=="123")
+ {
+  $_SESSION['password']=$pass;
+ }
+ else
+ {
+  $error="Incorrect Password";
+ }
+}
+
+if(isset($_POST['page_logout']))
+{
+ unset($_SESSION['password']);
+}
+?>
+
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+<link rel="stylesheet" type="text/css" href="password_style.css">
 </head>
 <body>
-  <form method="post" action="authenticate.php">
-    password: <input type="text" name="password"><br>
-    <input type="submit">
-  </form>
+<div id="wrapper">
 
+<?php
+if($_SESSION['password']=="123")
+{
+ ?>
+ <h1>Create Password Protected Webpage Using PHP, HTML And CSS</h1>
+ <form method="post" action="" id="logout_form">
+  <input type="submit" name="page_logout" value="LOGOUT">
+ </form>
+ <?php
+}
+else
+{
+ ?>
+ <form method="post" action="" id="login_form">
+  <h1>LOGIN TO PROCEED</h1>
+  <input type="password" name="pass" placeholder="*******">
+  <input type="submit" name="submit_pass" value="DO SUBMIT">
+  <p>"Password : 123"</p>
+  <p><font style="color:red;"><?php echo $error;?></font></p>
+ </form>
+ <?php	
+}
+?>
+
+</div>
 </body>
 </html>
