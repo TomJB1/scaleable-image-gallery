eb1601c72655f384f3972e10a650a7c813328078
Tom <tombrandisicloud@gmail.com>
2023-06-27 21:36:49 +0100

    change so acuall password is never sent to server

--- home.php
+++ home.php
@@ -9,13 +9,9 @@ session_start(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php
-    if (isset($_POST['password']) )
-    {
-        $_SESSION["password"] = $_POST["password"]; 
-    }
     
-    if(password_verify(($_SESSION["password"]), '$2y$10$FfEK/s4aowsb.HSWyMEmE.W9U.010ykPnP0tI17lhPjzp0dACyoie')): ?>
-    
+    if((password_verify(($_POST['password']), '$2y$10$FfEK/s4aowsb.HSWyMEmE.W9U.010ykPnP0tI17lhPjzp0dACyoie')) || $_SESSION["loggedIn"] == true): ?>
+    <?php $_SESSION["loggedIn"] = true;  ?>
     <link rel="stylesheet" type="text/css" href="home.css" />
     <script src="home.js" defer></script>
 </head>
