0eb6eda856bd64e67ebea4c8b89c84984aa6c243
Tom <tombrandisicloud@gmail.com>
2023-06-27 20:55:36 +0100

    center login form and change word on input

--- login.php
+++ login.php
@@ -4,11 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="login.css" />
 </head>
 <body>
   <form method="post" action="home.php">
     password: <input type="text" name="password"><br>
-    <input type="submit">
+    <input type="submit" value="Login">
   </form>
 
 </body>
