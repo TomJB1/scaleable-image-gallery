acc1a850c562dff4e2a494e2ff0b3ca123f655f9
Tom <tombrandisicloud@gmail.com>
2023-06-28 15:33:38 +0100

    delete extra to see if it is needed

--- directoryContent.php
+++ directoryContent.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
     $input = json_decode(file_get_contents('php://input'), true);
-    echo $input['test'];
-    echo "working" ?>
-</body>
-</html>
+    echo $input['test']; ?>
