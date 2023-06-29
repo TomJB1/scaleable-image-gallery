15511d855286b87f90d15b638e3c67ede00eabe2
Tom <tombrandisicloud@gmail.com>
2023-06-29 18:53:22 +0100

    break up output

--- directoryContent.php
+++ directoryContent.php
@@ -1,6 +1,8 @@
 <?php
     $input = json_decode(file_get_contents('php://input'), true);
     $fileArray = scandir(($input['dir']));
-    echo $fileArray[1]; 
+    echo $fileArray[1];
+    echo "--";
     echo $input;
+    echo "--";
     echo $input['dir'];?>
