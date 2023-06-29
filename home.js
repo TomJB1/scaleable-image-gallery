c77f89736ffe1a1151bea49844d12de9f2e26c9b
Tom <tombrandisicloud@gmail.com>
2023-06-29 19:20:59 +0100

    create cards automatically

---
+++ home.js
@@ -0,0 +1,25 @@
+let data = {"dir": "images"};
+let titles = [];
+const main = document.getElementById("main");
+
+fetch("directoryContent.php", {
+  method: "POST",
+  headers: {'Content-Type': 'application/json'}, 
+  body: JSON.stringify(data)
+}).then(res => {
+  console.log("Request complete! response:", res);
+  titles = res.split(',');
+});
+
+titles.forEach(title => {
+    const link = document.createElement("a");
+    link.href = "gallery.php#"+title;
+    link.innerHTML = `
+    <div class="tile">
+        <h1>${title}</h1>
+        <div class="imgDiv">
+            <img src="images/${title}/0.jpg">
+        </div>
+    </div>`
+    main.appendChild(link);
+});
