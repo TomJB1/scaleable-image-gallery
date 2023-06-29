1855120a04d89b5a76ea12cce357f8c1e339a724
Tom <tombrandisicloud@gmail.com>
2023-06-29 19:02:21 +0100

    try var_dump( )

--- directoryContent.php
+++ directoryContent.php
@@ -1,10 +1,4 @@
 <?php
     $input = json_decode(file_get_contents('php://input'), true);
     $fileArray = scandir(($input['dir']));
-    echo $fileArray[1];
-    echo "break";
-    echo $fileArray;
-    echo "break";
-    echo $input;
-    echo "break";
-    echo $input['dir'];?>
+    var_dump($fileArray);?>
