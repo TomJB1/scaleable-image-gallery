b3ffbb49b4a65b1aaed0695b915d1351a0728654
Tom <tombrandisicloud@gmail.com>
2023-06-29 19:05:54 +0100

    try implode( )

--- directoryContent.php
+++ directoryContent.php
@@ -1,4 +1,4 @@
 <?php
     $input = json_decode(file_get_contents('php://input'), true);
     $fileArray = scandir(($input['dir']));
-    var_dump($fileArray);?>
+    echo implode( ',', $fileArray);?>
