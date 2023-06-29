00af7447fcdf38dc3ec734c42d9028737fc51627
Tom <tombrandisicloud@gmail.com>
2023-06-29 18:51:04 +0100

    add more echos to troubleshoot

--- directoryContent.php
+++ directoryContent.php
@@ -1,4 +1,6 @@
 <?php
     $input = json_decode(file_get_contents('php://input'), true);
     $fileArray = scandir(($input['dir']));
-    echo $fileArray[1]; ?>
+    echo $fileArray[1]; 
+    echo $input;
+    echo $input['dir'];?>
