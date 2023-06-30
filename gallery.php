0334f12fd834bf4ebc4c339c03140730a8b1046f
Tom <tombrandisicloud@gmail.com>
2023-06-30 18:30:17 +0100

    change to absolute paths

--- gallery.php
+++ gallery.php
@@ -11,8 +11,8 @@
     if((password_verify(($_POST['password']), '$2y$10$FfEK/s4aowsb.HSWyMEmE.W9U.010ykPnP0tI17lhPjzp0dACyoie')) || $_SESSION["loggedIn"] == true): ?>
     <?php $_SESSION["loggedIn"] = true;  ?>
     
-    <link rel="stylesheet" type="text/css" href="styles.css" />
-    <script src="script.js" defer></script>
+    <link rel="stylesheet" type="text/css" href="/styles.css" />
+    <script src="/script.js" defer></script>
 </head>
 <body id="body">
     <div id="header">
