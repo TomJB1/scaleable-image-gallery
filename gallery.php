093baa56e9ce8c7fe1b23011fe2cf83646cbd4ed
Tom <tombrandisicloud@gmail.com>
2023-07-02 17:24:58 +0100

    change more links

--- gallery.php
+++ gallery.php
@@ -11,7 +11,7 @@
     if((password_verify(($_POST['password']), '$2y$10$FfEK/s4aowsb.HSWyMEmE.W9U.010ykPnP0tI17lhPjzp0dACyoie')) || $_SESSION["loggedIn"] == true): ?>
     <?php $_SESSION["loggedIn"] = true;  ?>
     
-    <link rel="stylesheet" type="text/css" href="/styles.css" />
+    <link rel="stylesheet" type="text/css" href="/stylesheets/gallery.css" />
     <script src="/script.js" defer></script>
 </head>
 <body id="body">
--- home.php
+++ home.php
@@ -12,7 +12,7 @@ session_start(); ?>
     
     if((password_verify(($_POST['password']), '$2y$10$FfEK/s4aowsb.HSWyMEmE.W9U.010ykPnP0tI17lhPjzp0dACyoie')) || $_SESSION["loggedIn"] == true): ?>
     <?php $_SESSION["loggedIn"] = true;  ?>
-    <link rel="stylesheet" type="text/css" href="home.css" />
+    <link rel="stylesheet" type="text/css" href="/stylesheets/home.css" />
     <script src="home.js" defer></script>
 </head>
 <body id="body">
