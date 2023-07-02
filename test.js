4e7dd838c2efcb9265084012d3f722054e8c0f40
Tom <tombrandisicloud@gmail.com>
2023-07-02 18:16:51 +0100

    delete testing files

--- test.js
+++
@@ -1,9 +0,0 @@
-let data = {"dir": "images"};
-
-fetch("directoryContent.php", {
-  method: "POST",
-  headers: {'Content-Type': 'application/json'}, 
-  body: JSON.stringify(data)
-}).then(res => {
-  console.log("Request complete! response:", res);
-});
