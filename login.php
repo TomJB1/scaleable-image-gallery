bd2ff9380cb7bb96d99bd995acb384e82e7a4e37
Tom <tombrandisicloud@gmail.com>
2023-07-02 17:19:58 +0100

    changed links

--- login.php
+++ login.php
@@ -4,12 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" type="text/css" href="login.css" />
+  <link rel="stylesheet" type="text/css" href="/login.css" />
   <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet"> 
 </head>
 <body>
   <div id="title">Brandis Photos</div>
-  <form method="post" action="home.php">
+  <form method="post" action="/">
     <h3>password:</h3>
     <input autocomplete="off" type="text" name="password"><br>
     <input type="submit" value="Login">
