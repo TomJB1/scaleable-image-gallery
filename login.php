7e588fb674164da81c95e30ade7b070c4ff9bc19
Tom <tombrandisicloud@gmail.com>
2023-07-02 17:23:55 +0100

    change location of stylesheets and link to them

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" type="text/css" href="/login.css" />
+  <link rel="stylesheet" type="text/css" href="/stylesheets/login.css" />
   <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet"> 
 </head>
 <body>
