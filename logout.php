91f49f68c24c70e596c2dbbb9ffcdeb8197be353
Tom <tombrandisicloud@gmail.com>
2023-07-02 17:32:10 +0100

    change some more links

--- logout.php
+++ logout.php
@@ -7,13 +7,13 @@ $_SESSION["loggedIn"] = false;  ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" type="text/css" href="login.css" />
+  <link rel="stylesheet" type="text/css" href="/stylesheets/login.css" />
   <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet"> 
 </head>
 <body>
 <?php if($_SESSION["loggedIn"] == false):?>
     <h1 id="title">Logged Out</h1>
-    <a href="login.php">Login</a>
+    <a href="/login">Login</a>
 <?php endif ?>
 </body>
 </html>
