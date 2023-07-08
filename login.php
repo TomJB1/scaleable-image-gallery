4bd7509e0b83c6d005a6fa91bd29021cbd9e4836
Tom <tombrandisicloud@gmail.com>
2023-07-08 12:23:43 +0100

    add favicon

--- login.php
+++ login.php
@@ -5,7 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
   <link rel="stylesheet" type="text/css" href="/stylesheets/login.css" />
-  <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet">
+
+  <link rel="icon" type="image/svg+xml" href="/assets/images/favicon.svg">
+  <link rel="icon" type="image/png" href="/assets/images/favicon.png">
 </head>
 <body>
   <div id="title">Brandis Photos</div>
--- logout.php
+++ logout.php
@@ -8,7 +8,10 @@ $_SESSION["loggedIn"] = false;  ?>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Logout</title>
   <link rel="stylesheet" type="text/css" href="/stylesheets/login.css" />
-  <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet">
+
+  <link rel="icon" type="image/svg+xml" href="/assets/images/favicon.svg">
+  <link rel="icon" type="image/png" href="/assets/images/favicon.png">
 </head>
 <body>
 <?php if($_SESSION["loggedIn"] == false):?>
